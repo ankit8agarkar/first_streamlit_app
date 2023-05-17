84cbe03bae01cc621a1a9d180baee787e55af067
ankit8agarkar <68893180+ankit8agarkar@users.noreply.github.com>
2023-05-17 23:19:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,21 +27,22 @@ streamlit.dataframe(fruits_to_show)
 
 
 def get_fruityvice_data(this_fruit_choice):
-  streamlit.write('The user entered ', fruit_choice)
-   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice )
-   # normalizing the json
-   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.write('The user entered ', fruit_choice)
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+    # normalizing the json
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     return fruityvice_normalized
   
- #New section to show fruityvice api response
+# New section to show fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 try:
-  fruit_choice = streamlit.text_input('What fruit would you like information about?')
-  if not fruit_choice:
-    streamlit.error('Please select a fruit to get information')
-else:
-  back_from_function = get_fruityvice_data(fruit_choice)
-  streamlit.dataframe(back_from_function)
+    fruit_choice = streamlit.text_input('What fruit would you like information about?')
+    if not fruit_choice:
+        streamlit.error('Please select a fruit to get information')
+    else:
+        back_from_function = get_fruityvice_data(fruit_choice)
+        streamlit.dataframe(back_from_function)
+
   
 """
 streamlit.header("Fruityvice Fruit Advice!")
